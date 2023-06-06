2ce5ba16f8461184ca350a3b38a6d82db9dc99a3
Axel Padilla <adpadillar25@gmail.com>
2023-06-06 11:25:57 -0600

    initial commit

---
+++ include/Rectangular.h
@@ -0,0 +1,15 @@
+#ifndef RECTANGULAR_H
+#define RECTANGULAR_H
+
+class Rectangular {
+    private:
+        double x, y;
+
+    public:
+        Rectangular();
+        Rectangular(double x, double y);
+};
+
+
+#endif // !RECTANGULAR_H
+
---
+++ src/Rectangular.cpp
@@ -0,0 +1,9 @@
+#include "../include/Rectangular.h"
+
+
+Rectangular::Rectangular(): Rectangular(1, 1) {}
+
+Rectangular::Rectangular(double x, double y) {
+    this->x = x;
+    this->y = y;
+};
